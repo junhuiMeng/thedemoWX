24a7b75d00e70c09f62727d89de1d51bc67075c5
meng <1076135320@qq.com>
2019-12-30 09:00:22 +0800

    第一次提交，微信版

---
+++ js/base.js
@@ -0,0 +1,5 @@
+//window.url = "http://192.168.43.118:8888/a";
+//生产环境
+//window.url = "http://www.nodesoft.cn/yademocracygrade/api";
+//本地环境
+window.url = "http://192.168.1.146:9000/yademocracygrade/api";
